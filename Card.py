afec4fe79e6e906d0321e71a3eeebf40e13760a5
XiaoshuaiGeng <genglittlehandsome@hotmail.com>
2020-11-26 03:57:13 -0500

    initial setup on Card class

--- Card.py
+++ Card.py
@@ -1,11 +1,24 @@
+# CARD
+# SUIT, RANK, VALUE
+
+import random
+
+suits = ('Hearts','Diamonds','Spades','Clubs')
+ranks = ('Two' 'Three', 'Four', 'Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace')
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five':5,'Six': 6,
+    'Seven':7,'Eight': 8,'Nine': 9,'Ten':10,'Jack':11,'Queen':12,'King':13,'Ace':14}
+
 class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+
+    def __init__(self, suit:str, rank:str):
+        self.suit = suit.capitalize()
+        self.rank = rank.capitalize()
+        self.value = values[self.rank]
     
     def __str__(self) -> str:
         return self.rank + " of " + self.suit
 
-    values = {"Two": }
+
 two_hearts = Card("Hearts", "Two")
-print(two_hearts)
+three_of_clubs = Card('Clubs','Three')
+print(two_hearts, three_of_clubs)
